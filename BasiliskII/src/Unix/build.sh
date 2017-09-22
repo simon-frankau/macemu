7b9bf0bc7df430b1b9e5795ec4d68579238b7ef4
Ricky Zhang <rickyzhang@gmail.com>
2017-09-22 15:06:58 -0400

    Change build script
    Signed-off-by: Ricky Zhang <rickyzhang@gmail.com>

--- BasiliskII/src/Unix/build.sh
+++ BasiliskII/src/Unix/build.sh
@@ -1,4 +1,4 @@
 make clean
 NO_CONFIGURE=1 ./autogen.sh
-./configure --enable-sdl-video --enable-sdl-audio --disable-vosf --disable-jit-compiler --with-x --with-gtk --with-mon
+./configure --enable-addressing=banks --with-gtk=no --with-esd=no
 make -j 32
